f3d6e5b42e8876e13385db615689731ab673a415
dea-1611 <dea.ws1611@gmail.com>
2025-08-14 01:48:59 +0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,8 @@
-class Urls:
-    URL_basic = 'https://qa-scooter.praktikum-services.ru/'
-    URL_courier_create = f'{URL_basic}api/v1/courier/'
-    URL_courier_login = f'{URL_basic}api/v1/courier/login'
-    URL_orders_create = f'{URL_basic}api/v1/orders'
+BASE_URL = 'https://qa-scooter.praktikum-services.ru/api/v1'
+
+URL_COURIER_CREATE = f'{BASE_URL}/courier'
+URL_COURIER_LOGIN = f'{BASE_URL}/courier/login'
+URL_COURIER_DELETE = f'{BASE_URL}/courier'
+URL_ORDERS_CREATE = f'{BASE_URL}/orders'
+URL_ORDERS_LIST = f'{BASE_URL}/orders'
+URL_ORDERS_TRACK = f'{BASE_URL}/orders/track'
