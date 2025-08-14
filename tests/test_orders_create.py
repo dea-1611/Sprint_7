103a3d8b6ffec02ed9416ccd7c1ed164e3559798
dea-1611 <dea.ws1611@gmail.com>
2025-08-14 16:47:55 +0500

    Update test_orders_create.py

--- tests/test_orders_create.py
+++ tests/test_orders_create.py
@@ -1,30 +1,29 @@
-import requests
 import allure
-import pytest
-from ..data import OrderData
-from ..urls import URL_ORDERS_CREATE
+from Sprint_7.data import OrderData
+from Sprint_7.api import OrderAPI
 
 class TestOrderCreation:
     @allure.title('Создание заказа с серым самокатом')
     def test_create_order_grey(self):
-        response = requests.post(URL_ORDERS_CREATE, json=OrderData.order_data_grey_1)
+        response = OrderAPI.create_order(OrderData.order_data_grey_1)
         assert response.status_code == 201
         assert 'track' in response.json()
 
     @allure.title('Создание заказа с черным самокатом')
     def test_create_order_black(self):
-        response = requests.post(URL_ORDERS_CREATE, json=OrderData.order_data_black_2)
+        response = OrderAPI.create_order(OrderData.order_data_black_2)
         assert response.status_code == 201
         assert 'track' in response.json()
 
     @allure.title('Создание заказа с двумя цветами')
     def test_create_order_two_colors(self):
-        response = requests.post(URL_ORDERS_CREATE, json=OrderData.order_data_two_colors_3)
+        response = OrderAPI.create_order(OrderData.order_data_two_colors_3)
         assert response.status_code == 201
         assert 'track' in response.json()
 
     @allure.title('Создание заказа без указания цвета')
     def test_create_order_no_colors(self):
-        response = requests.post(URL_ORDERS_CREATE, json=OrderData.order_data_no_colors_4)
+        response = OrderAPI.create_order(OrderData.order_data_no_colors_4)
         assert response.status_code == 201
         assert 'track' in response.json()
+
