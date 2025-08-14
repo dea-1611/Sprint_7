cdc47bb94d7bdeaff629009f16fcc8dfe53c42eb
dea-1611 <dea.ws1611@gmail.com>
2025-08-15 00:01:35 +0500

    Update test_orders_list.py

--- tests/test_orders_list.py
+++ tests/test_orders_list.py
@@ -3,6 +3,7 @@ from Sprint_7.api import OrderAPI
 
 class TestOrdersList:
     @allure.title('Получение списка заказов')
+    @allure.step('Запрос списка всех заказов. Проверка кода и тела ответа.')
     def test_get_orders_list(self):
         response = OrderAPI.get_orders_list()
         assert response.status_code == 200
