4d42758033b0f5b561c79aefbb4a24b5f2eb2de7
dea-1611 <dea.ws1611@gmail.com>
2025-08-15 00:00:54 +0500

    Update test_courier_login.py

--- tests/test_courier_login.py
+++ tests/test_courier_login.py
@@ -5,6 +5,7 @@ from Sprint_7.api import CourierAPI
 
 class TestCourierLogin:
     @allure.title('Успешная аутентификация курьера')
+    @allure.step('Авторизация с валидными данными. Проверка кода и тела ответа.')
     def test_courier_login_success(self):
         response = CourierAPI.login_courier({
             'login': Data.valid_login,
@@ -14,6 +15,7 @@ class TestCourierLogin:
         assert 'id' in response.json()
 
     @allure.title('Аутентификация с неверным логином')
+    @allure.step('Попытка входа с невалидным логином. Проверка кода и тела ответа.')
     def test_courier_login_invalid_login(self):
         response = CourierAPI.login_courier({
             'login': 'invalid_login',
@@ -23,6 +25,7 @@ class TestCourierLogin:
         assert response.json()['message'] == ErrorMessages.ACCOUNT_NOT_FOUND
 
     @allure.title('Аутентификация с неверным паролем')
+    @allure.step('Попытка входа с невалидным паролем. Проверка кода и тела ответа.')
     def test_courier_login_invalid_password(self):
         response = CourierAPI.login_courier({
             'login': Data.valid_login,
@@ -31,16 +34,23 @@ class TestCourierLogin:
         assert response.status_code == 404
         assert response.json()['message'] == ErrorMessages.ACCOUNT_NOT_FOUND
 
-    @allure.title('Аутентификация без логина')
-    def test_courier_login_missing_login(self):
-        response = CourierAPI.login_courier({'password': Data.valid_password})
+    @allure.title('Аутентификация с пустым логином')
+    @allure.step('Попытка входа с пустым логином. Проверка кода и тела ответа.')
+    def test_courier_login_empty_login(self):
+        response = CourierAPI.login_courier({
+            'login': '',
+            'password': Data.valid_password
+        })
         assert response.status_code == 400
         assert response.json()['message'] == ErrorMessages.NOT_ENOUGH_DATA_FOR_LOGIN
 
-    @allure.title('Аутентификация без пароля')
-    def test_courier_login_missing_password(self):
-        response = CourierAPI.login_courier({'login': Data.valid_login})
-        assert response.status_code in [400, 504]
-        if response.status_code == 400:
-            assert response.json()['message'] == ErrorMessages.NOT_ENOUGH_DATA_FOR_LOGIN
+    @allure.title('Аутентификация с пустым паролем')
+    @allure.step('Попытка входа с пустым паролем. Проверка кода и тела ответа.')
+    def test_courier_login_empty_password(self):
+        response = CourierAPI.login_courier({
+            'login': Data.valid_login,
+            'password': ''
+        })
+        assert response.status_code == 400
+        assert response.json()['message'] == ErrorMessages.NOT_ENOUGH_DATA_FOR_LOGIN
 
